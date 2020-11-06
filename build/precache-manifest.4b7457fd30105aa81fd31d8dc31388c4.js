abcf14ca13dec6aa414d2158cef2b4d8b5d35f95
schak83 <bavibm@gmail.com>
2020-11-06 14:55:04 -0600

    first time git commit

---
+++ build/precache-manifest.4b7457fd30105aa81fd31d8dc31388c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05505450afc862eb7ad9b8805d6f00bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa418d3791a032ff8b7",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa418d3791a032ff8b7",
+    "url": "/static/js/main.f91eefc9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
